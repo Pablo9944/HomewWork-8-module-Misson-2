84aaf5bd2640603a7481735c29fb7f7973f7bf3f
Павел Юров <peyurov@gmail.com>
2022-05-06 12:30:33 +0300

    Добавьте файлы проекта.

---
+++ HomewWork 8 module Misson 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomewWork_8_module_Misson_2
+{
+    internal class Program
+    {
+        const string Path = @"F:\";
+        
+        static void Main(string[] args)
+        {
+
+            DirectoryInfo DI = new DirectoryInfo(Path);
+            long size = 0;
+            Size(DI,ref size);
+            Console.WriteLine(size);
+
+        }
+    
+    
+        static long Size (DirectoryInfo DI,ref long size)
+        {
+
+            try
+            {
+
+                FileInfo[] FI = DI.GetFiles();
+
+                foreach (var f in FI)
+                {
+                    size += f.Length;
+                }
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Есть файлы без доступа, честно не знаю как на Windows это сделать,что бы в каждую скрутыю папку вошел");
+            }
+
+
+            try
+            {
+                DirectoryInfo[] dir = DI.GetDirectories();
+                foreach (var item in dir)
+                {
+                    size += Size(item, ref size);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Есть папки без доступа,честно не знаю как на Windows это сделать,что бы в каждую скрутыю папку вошел");
+            }
+            return size;
+
+
+
+        }
+    
+    
+    }
+}
