039e2872ba7c5a42dd8c8ff77222b4c63bf02e6f
Павел Юров <peyurov@gmail.com>
2022-05-06 18:50:27 +0300

    UP

--- HomewWork 8 module Misson 2/Program.cs
+++ HomewWork 8 module Misson 2/Program.cs
@@ -16,25 +16,6 @@ namespace HomewWork_8_module_Misson_2
         {
 
 
-            DriveInfo[] dv = DriveInfo.GetDrives();
-            string[] myArray = new string[dv.Length];
-            foreach (DriveInfo d in dv)
-            {
-                
-               
-                for (int i = 0; i < dv.Length; i++)
-                {
-                    myArray[i] = d.Name;
-                }
-                
-            }
-
-            foreach (var item in myArray)
-            {
-                Console.WriteLine(item);
-            }
-
-
             DirectoryInfo DI = new DirectoryInfo(Path);
             long size = 0;
             Size(DI,ref size);
